c1142ffc1ef6f58c132201b214ed1aed39dae6a4
dlipdhunter <dlipdhunter@gmail.com>
2018-01-17 21:31:20 +0530

    Added form for adding a new member to the crew

--- ClientApp/app/components/strawhats-crew/add-strawhat/add-strawhat.component.ts
+++ ClientApp/app/components/strawhats-crew/add-strawhat/add-strawhat.component.ts
@@ -1,9 +1,28 @@
+// Template driven form
+
 import { Component } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { Pirate } from "../models/pirate";
 
 @Component({
     selector: 'add-strawhat',
     templateUrl: './add-strawhat.component.html'
 })
 export class AddStrawHatComponent {
+    strawhat: Pirate;
 
+    /**
+     *
+     */
+    constructor() {
+        this.strawhat = {
+            id: 0,
+            name: "",
+            nickName: "",
+            position: "",
+            crewName: "Straw Hat Pirates",
+            bounty: 0
+        }
+        
+    }
 }
